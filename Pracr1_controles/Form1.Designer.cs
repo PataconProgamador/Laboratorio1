2700534df10425c73af41dccb4268369fd887f8b
PataconProgamador <luiscarlos.jimenez@utp.ac.pa>
2026-08-24 11:15:55 -0500

    Add files via upload

---
+++ Pracr1_controles/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Pracr1_controles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_1 = new System.Windows.Forms.Label();
+            this.txt_1 = new System.Windows.Forms.TextBox();
+            this.lbl_2 = new System.Windows.Forms.Label();
+            this.txt_2 = new System.Windows.Forms.TextBox();
+            this.lbl_3 = new System.Windows.Forms.Label();
+            this.txt_3 = new System.Windows.Forms.TextBox();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.lbl_4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_1
+            // 
+            this.lbl_1.AutoSize = true;
+            this.lbl_1.Location = new System.Drawing.Point(45, 35);
+            this.lbl_1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lbl_1.Name = "lbl_1";
+            this.lbl_1.Size = new System.Drawing.Size(44, 25);
+            this.lbl_1.TabIndex = 0;
+            this.lbl_1.Text = "Día";
+            this.lbl_1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txt_1
+            // 
+            this.txt_1.Location = new System.Drawing.Point(185, 35);
+            this.txt_1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txt_1.Name = "txt_1";
+            this.txt_1.Size = new System.Drawing.Size(196, 31);
+            this.txt_1.TabIndex = 1;
+            // 
+            // lbl_2
+            // 
+            this.lbl_2.AutoSize = true;
+            this.lbl_2.Location = new System.Drawing.Point(45, 112);
+            this.lbl_2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lbl_2.Name = "lbl_2";
+            this.lbl_2.Size = new System.Drawing.Size(53, 25);
+            this.lbl_2.TabIndex = 2;
+            this.lbl_2.Text = "Mes";
+            // 
+            // txt_2
+            // 
+            this.txt_2.Location = new System.Drawing.Point(185, 99);
+            this.txt_2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txt_2.Name = "txt_2";
+            this.txt_2.Size = new System.Drawing.Size(196, 31);
+            this.txt_2.TabIndex = 3;
+            // 
+            // lbl_3
+            // 
+            this.lbl_3.AutoSize = true;
+            this.lbl_3.Location = new System.Drawing.Point(45, 189);
+            this.lbl_3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lbl_3.Name = "lbl_3";
+            this.lbl_3.Size = new System.Drawing.Size(50, 25);
+            this.lbl_3.TabIndex = 4;
+            this.lbl_3.Text = "Año";
+            // 
+            // txt_3
+            // 
+            this.txt_3.Location = new System.Drawing.Point(185, 174);
+            this.txt_3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txt_3.Name = "txt_3";
+            this.txt_3.Size = new System.Drawing.Size(196, 31);
+            this.txt_3.TabIndex = 5;
+            // 
+            // btn_1
+            // 
+            this.btn_1.Location = new System.Drawing.Point(71, 283);
+            this.btn_1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(150, 44);
+            this.btn_1.TabIndex = 6;
+            this.btn_1.Text = "Enviar";
+            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // btn_2
+            // 
+            this.btn_2.Location = new System.Drawing.Point(299, 283);
+            this.btn_2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(150, 44);
+            this.btn_2.TabIndex = 7;
+            this.btn_2.Text = "Cerrar";
+            this.btn_2.UseVisualStyleBackColor = true;
+            this.btn_2.Click += new System.EventHandler(this.btn_2_Click);
+            // 
+            // lbl_4
+            // 
+            this.lbl_4.AutoSize = true;
+            this.lbl_4.Location = new System.Drawing.Point(532, 105);
+            this.lbl_4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lbl_4.Name = "lbl_4";
+            this.lbl_4.Size = new System.Drawing.Size(130, 25);
+            this.lbl_4.TabIndex = 8;
+            this.lbl_4.Text = "La fecha es:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 372);
+            this.Controls.Add(this.lbl_4);
+            this.Controls.Add(this.btn_2);
+            this.Controls.Add(this.btn_1);
+            this.Controls.Add(this.txt_3);
+            this.Controls.Add(this.lbl_3);
+            this.Controls.Add(this.txt_2);
+            this.Controls.Add(this.lbl_2);
+            this.Controls.Add(this.txt_1);
+            this.Controls.Add(this.lbl_1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_1;
+        private System.Windows.Forms.TextBox txt_1;
+        private System.Windows.Forms.Label lbl_2;
+        private System.Windows.Forms.TextBox txt_2;
+        private System.Windows.Forms.Label lbl_3;
+        private System.Windows.Forms.TextBox txt_3;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Label lbl_4;
+    }
+}
+
---
+++ Pracr1_controles/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pracr1_controles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txt_1.Text) || string.IsNullOrWhiteSpace(txt_2.Text) || string.IsNullOrWhiteSpace(txt_3.Text))
+            {
+                MessageBox.Show("Campos no rellenos cerrando ventana.", "Error.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+            }
+            else
+            {
+                lbl_4.Text = "La fecha es:" + txt_1.Text + "/" + txt_2.Text + "/" + txt_3.Text;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("cerrando ventana.", "Cerrar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            this.Close();
+        }
+    }
+}
